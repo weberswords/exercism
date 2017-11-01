84cea2f7e4d1e347e20687b6547171b92e40ece4
Stephanie Weber <sweber@thoughtworks.com>
2017-11-01 10:57:55 -0400

    [pangram] phraseMissingXFails

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -2,7 +2,10 @@ public class PangramChecker {
 
     public boolean isPangram(String input) {
 
-        return (input == "") ? false : true;
-
+        input = input.toLowerCase();
+        return input.chars()
+                .filter(i -> i >= 'a' && i <= 'z')
+                .distinct()
+                .count() == 26 ? true : false;
     }
 }
--- java/pangram/src/test/java/PangramCheckerTest.java
+++ java/pangram/src/test/java/PangramCheckerTest.java
@@ -24,7 +24,6 @@ public class PangramCheckerTest {
         assertTrue(pangramChecker.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void phraseMissingCharacterXIsNotPangram() {
         assertFalse(pangramChecker.isPangram("a quick movement of the enemy will jeopardize five gunboats"));
