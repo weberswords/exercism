e3476feb005c92cd790f2726d9a6c1d43d3cf9b1
Stephanie Weber <sweber@thoughtworks.com>
2017-11-01 10:35:20 -0400

    [pangram] emptySentenceIsNotPangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,8 @@
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        return false;
     }
 
 }
