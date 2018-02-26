e67f853c5f0e937914d02e8cf741fd9fa8ee9aaf
Stephanie Weber <sweber@thoughtworks.com>
2018-02-25 22:27:34 -0500

    leap solved

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,3 +1,15 @@
 object Leap {
-  def leapYear(year: Int): Boolean = ???
+  def leapYear(year: Int): Boolean = {
+    val yearDividedBy4 = year % 4
+    val yearDividedBy100 = year % 100
+    val yearDividedBy400 = year % 400
+
+    if (yearDividedBy4 == 0 && yearDividedBy100 != 0) {
+      true
+    } else if (yearDividedBy4 != 0 || yearDividedBy400 != 0)  {
+      false
+    } else {
+      true
+    }
+  }
 }
