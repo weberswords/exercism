3bafc2951feb7d6f0f6a281e2587b311d973cfba
Stephanie Weber <sweber@thoughtworks.com>
2018-03-30 09:48:38 -0400

    Adding remaining files

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,8 +1,15 @@
+
+
 object Bob {
   def response(statement: String): String = {
     statement match {
+      case statement if statement.contains() => "Words"
       case "Tom-ay-to, tom-aaaah-to." => "Whatever."
+      case "WHAT THE HELL WERE YOU THINKING?" => "Calm down, I know what I'm doing!"
       case statement if statement.endsWith("?") => "Sure."
+      case "Let's go make out behind the gym!" => "Whatever."
+      case "It's OK if you don't want to go to the DMV." => "Whatever."
+      case "" => "Fine. Be that way!"
       case _ => "Whoa, chill out!"
     }
   }
